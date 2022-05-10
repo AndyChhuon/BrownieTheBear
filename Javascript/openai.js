dca330d3515d5e63c9008758648b89038badbe20
AndyChhuon <chhuonandy@gmail.com>
2022-05-10 12:13:20 -0400

    Added data and prompt inputs + display engine list

--- Javascript/openai.js
+++ Javascript/openai.js
@@ -1,4 +1,12 @@
-/*const data = {
+
+
+var engine_html ="<select name='engines' id='engines' class='short_list'>";
+
+async function completions(){
+  var apiKey = document.getElementById("apiKey").value;
+  var engine = document.getElementById("engine_options").value
+
+  const data = {
     prompt: "Write a poem about a dog wearing skis",
     temperature: 0.5,
     max_tokens: 64,
@@ -7,16 +15,49 @@
     presence_penalty: 0.0,
    };
 
-async function openai(){
-    await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    await fetch("https://api.openai.com/v1/engines/"+engine+"/completions", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": "Bearer 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",
+          "Authorization": "Bearer " + apiKey,
         },
         body: JSON.stringify(data),
        }).then(response => response.json())
-       .then(data => console.log(data.text));
+       .then(data => console.log(data));
 }
-openai()
+
+async function getEngines(){ //Get list of engines from API and display as select options
+  var apiKey = document.getElementById("apiKey").value;
+  console.log(apiKey)
+  await fetch("https://api.openai.com/v1/engines", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + apiKey,
+    },
+   }).then(response => response.json())
+   .then(data => {
+     console.log(data)
+     var EngineKeys = Object.keys(data["data"])
+     EngineKeys.forEach(element => {
+      engine_html += "<option value='"+data["data"][element]["id"]+"'>"+data["data"][element]["id"]+"</option>"
+    });
+   });
+   engine_html += "</select>"
+   document.getElementById('engine_options').innerHTML = engine_html;
+
+
+   
+}
+
+function change_prompt_height(){
+
+}
+
+window.onload = function(){
+  getEngines()
+ }
+
+/*
+Note: For HTTP errors we can check the response. ok property to see if the request failed and reject the promise ourselves by calling return Promise
 */
